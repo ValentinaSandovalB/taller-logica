bc56ad60d242b53601e71c35d43048aaecc18023
valentina sandoval buitrago <lilithasamililithasami@gmail.com>
2023-08-15 14:44:22 -0500

    taller logica
    el que sirve

--- Taller_Logica/src/taller/solucion.java
+++ Taller_Logica/src/taller/solucion.java
@@ -2,12 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package empresas;
+package taller;
 
 /**
  *
- * @author 505
+ * @author Valentina
  */
-public class Empresa {
-    
+public class solucion {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        
+    }
 }
